ece14f6056c532db2cf8a25625336685ecc5420f
Timothy J. Wood <tjw@me.com>
2016-03-11 05:31:37 +0000

    The module importing doesn't work as well as I'd hoped.

--- DotStar.swift
+++ DotStar.swift
@@ -1,12 +1,22 @@
 // This will locate the module.modulemap in our `modules` directory and will build a Swift module based on <linux/spidev.h>
 
+import Glibc
 import SPI
 
+// Basic defines come through the module, but anything at all interesting doesn't. Not sure if there is a workaround for this or not...
+let SPI_MODE_0:UInt = 0
+let SPI_IOC_WR_MODE:UInt = 0x40016b01
+let SPI_IOC_WR_MAX_SPEED_HZ:UInt = 0x40046b04
+
 class DotStar {
+    enum Error: ErrorType {
+        case CannotOpen
+    }
+
     let pixelCount:Int
     let bitrate:Int
 
-    var fd:CInt? = -1
+    var fd:CInt = -1
 
     // TODO: Make the order configurable? Default xBRG.
     private let rOffset = 2
@@ -26,11 +36,25 @@ class DotStar {
         self.pixelCount = pixelCount
         self.bitrate = bitrate
 
-	pixels = Array<UInt8>(count: 4*pixelCount, repeatedValue: 0)
+	// Initialize the packed pixels all to 0xff000000
+	self.bytes = Array<UInt8>(count: 4*pixelCount, repeatedValue: 0)
 	for pixelIndex in 0..<pixelCount {
-	    pixels[4*pixelIndex + 0] = 0xff
+	    self.bytes[4*pixelIndex + 0] = 0xff
+	}
+    }
+
+    func begin() throws {
+        if fd != -1 {
+	    abort() // Throw an error
+	}
+	fd = open("/dev/spidev0.0", O_RDWR)
+	guard fd >= 0 else {
+	    throw Error.CannotOpen
 	}
 
+        let mode = SPI_MODE_0 | SPI_NO_CS
+	ioctl(fd, SPI_IOC_WR_MODE, &mode)
+        ioctl(fd, SPI_IOC_WR_MAX_SPEED_HZ, bitrate)
     }
 }
 
---
+++ spi.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <linux/spi/spidev.h>
+
+int main(int argc, char *argv[])
+{
+  printf("let SPI_MODE_0:UInt = %d\n", SPI_MODE_0);
+  printf("let SPI_IOC_WR_MODE:UInt = 0x%x\n", SPI_IOC_WR_MODE);
+  printf("let SPI_IOC_WR_MAX_SPEED_HZ:UInt = 0x%x\n", SPI_IOC_WR_MAX_SPEED_HZ);
+
+  return 0;
+}
