9d4cb5d089347e30730959e6ad7b1e174bbbf803
Timothy J. Wood <tjw@omnigroup.com>
2016-03-11 19:51:08 -0800

    Fixed indentation.

--- DotStar.swift
+++ DotStar.swift
@@ -21,8 +21,8 @@ class DotStar {
     // The public-facing pixel
     struct Pixel {
         let r:UInt8
-	let g:UInt8
-	let b:UInt8
+        let g:UInt8
+        let b:UInt8
     }
 
     private var bytes:[UInt8]
@@ -31,11 +31,11 @@ class DotStar {
         self.pixelCount = pixelCount
         self.bitrate = bitrate
 
-	// Initialize the packed pixels all to 0xff000000
-	self.bytes = Array<UInt8>(count: 4*pixelCount, repeatedValue: 0)
-	for pixelIndex in 0..<pixelCount {
-	    self.bytes[4*pixelIndex + 0] = 0xff
-	}
+        // Initialize the packed pixels all to 0xff000000
+        self.bytes = Array<UInt8>(count: 4*pixelCount, repeatedValue: 0)
+        for pixelIndex in 0..<pixelCount {
+            self.bytes[4*pixelIndex + 0] = 0xff
+        }
     }
 
     deinit {
@@ -44,61 +44,61 @@ class DotStar {
 
     func begin() throws {
         if fd != -1 {
-	    abort() // Throw an error
-	}
-	fd = open("/dev/spidev0.0", O_RDWR)
-	guard fd >= 0 else {
-	    throw Error.CannotOpen
-	}
+            abort() // Throw an error
+        }
+        fd = open("/dev/spidev0.0", O_RDWR)
+        guard fd >= 0 else {
+            throw Error.CannotOpen
+        }
 
         var mode:CUnsignedLong = Swift_SPI_MODE_0 | Swift_SPI_NO_CS
-	Swift_ioctl_v1(fd, Swift_SPI_IOC_WR_MODE, &mode)
+        Swift_ioctl_v1(fd, Swift_SPI_IOC_WR_MODE, &mode)
         Swift_ioctl_i1(fd, Swift_SPI_IOC_WR_MAX_SPEED_HZ, Int32(bitrate))
     }
 
     func end() {
-    	 if fd >= 0 {
-	    close(fd)
-	    fd = -1
-	 }
+        if fd >= 0 {
+            close(fd)
+            fd = -1
+        }
     }
 
     subscript(pixelIndex:Int) -> Pixel {
         get {
-	    let r = bytes[4*pixelIndex + rOffset]
-	    let g = bytes[4*pixelIndex + gOffset]
-	    let b = bytes[4*pixelIndex + bOffset]
-	    return Pixel(r:r, g:g, b:b)
-	}
-	set {
-	    bytes[4*pixelIndex + rOffset] = newValue.r
-	    bytes[4*pixelIndex + gOffset] = newValue.g
-	    bytes[4*pixelIndex + bOffset] = newValue.b
-	}
+            let r = bytes[4*pixelIndex + rOffset]
+            let g = bytes[4*pixelIndex + gOffset]
+            let b = bytes[4*pixelIndex + bOffset]
+            return Pixel(r:r, g:g, b:b)
+        }
+        set {
+            bytes[4*pixelIndex + rOffset] = newValue.r
+            bytes[4*pixelIndex + gOffset] = newValue.g
+            bytes[4*pixelIndex + bOffset] = newValue.b
+        }
     }
 
     func write() throws {
-    	 var xfer = Array<spi_ioc_transfer>()
+        var xfer = Array<spi_ioc_transfer>()
 
-	 var header = make_xfer()
-	 header.len = 4
-	 xfer.append(header)
+        var header = make_xfer()
+        header.len = 4
+        xfer.append(header)
 
-	 var data = make_xfer()
-	 data.tx_buf = Swift_SPI_pointer_to_integer(&bytes) // TODO: Use the unsafe buffer methods? Maybe not needed since this is a member of us and we are still alive
-	 data.len = UInt32(4*pixelCount)
-	 xfer.append(data)
+        var data = make_xfer()
+        data.tx_buf = Swift_SPI_pointer_to_integer(&bytes) // TODO: Use the unsafe buffer methods? Maybe not needed since this is a member of us and we are still alive
+        data.len = UInt32(4*pixelCount)
+        xfer.append(data)
 
-	 var trailer = make_xfer()
-	 trailer.len = (UInt32(pixelCount) + 15) / 16
-	 xfer.append(trailer)
+        var trailer = make_xfer()
+        trailer.len = (UInt32(pixelCount) + 15) / 16
+        xfer.append(trailer)
 
-	 Swift_ioctl_v1(fd, Swift_SPI_IOC_MESSAGE(3), &xfer)
+        Swift_ioctl_v1(fd, Swift_SPI_IOC_MESSAGE(3), &xfer)
     }
 
     private func make_xfer() -> spi_ioc_transfer {
-         return spi_ioc_transfer(tx_buf: 0, rx_buf:0, len: 0, speed_hz:bitrate,
-                                 delay_usecs: 0, bits_per_word: 8, cs_change: 0, tx_nbits: 0, rx_nbits: 0, pad: 0)
+        return spi_ioc_transfer(tx_buf: 0, rx_buf:0, len: 0, speed_hz:bitrate,
+        delay_usecs: 0, bits_per_word: 8, cs_change: 0, tx_nbits: 0, rx_nbits: 0, pad: 0)
     }
 }
 
@@ -107,7 +107,7 @@ extension DotStar {
     func clear() {
         let off = Pixel(r:0, g:0, b:0)
         for pixelIndex in 0..<pixelCount {
-	    self[pixelIndex] = off
-	}
+            self[pixelIndex] = off
+        }
     }
 }
--- SwiftSPI/spi.c
+++ SwiftSPI/spi.c
@@ -7,20 +7,20 @@ unsigned long Swift_SPI_IOC_WR_MAX_SPEED_HZ = SPI_IOC_WR_MAX_SPEED_HZ;
 
 unsigned long Swift_SPI_IOC_MESSAGE(int arg)
 {
-  return SPI_IOC_MESSAGE(arg);
+    return SPI_IOC_MESSAGE(arg);
 }
 
 int Swift_ioctl_v1(int fd, unsigned long request, void *arg)
 {
-  return ioctl(fd, request, arg);
+    return ioctl(fd, request, arg);
 }
 
 int Swift_ioctl_i1(int fd, unsigned long request, int arg)
 {
-  return ioctl(fd, request, arg);
+    return ioctl(fd, request, arg);
 }
 
 __u64 Swift_SPI_pointer_to_integer(void *ptr)
 {
-  return (__u64)ptr;
+    return (__u64)ptr;
 }
