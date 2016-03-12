2722b4c47806cc9f80db54f8b33a6a5e8526c62b
Timothy J. Wood <tjw@me.com>
2016-03-12 03:58:00 +0000

    Cycle some colors, in theory.

--- examples/onoff/main.swift
+++ examples/onoff/main.swift
@@ -1,6 +1,24 @@
+import Glibc
 
 let dotstar = DotStar(pixelCount:1)
 print("dotstar = \(dotstar)")
 
+let red = DotStar.Pixel(r:255, g:0, b:0)
+let green = DotStar.Pixel(r:0, g:255, b:0)
+let blue = DotStar.Pixel(r:0, g:0, b:255)
+
 try! dotstar.begin()
 
+while (true) {
+    dotstar[0] = red
+    try! dotstar.write()
+    usleep(200)
+
+    dotstar[0] = green
+    try! dotstar.write()
+    usleep(200)
+
+    dotstar[0] = blue
+    try! dotstar.write()
+    usleep(200)
+}
